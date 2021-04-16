abdceacb33f4f3b3222647cd1edcad56882f370a
hochan222 <hochan049@gmail.com>
2021-04-16 11:57:07 +0900

    [REFACTOR] change to promise.all()

--- routes/users.js
+++ routes/users.js
@@ -8,23 +8,6 @@ const ObjectUtils = require('../common/ObjectUtils');
 
 const END_POINT_42_API = "https://api.intra.42.fr";
 
-const get42UserCoalition = async (username, headers) => {
-  const coalitionUri = `${END_POINT_42_API}/v2/users/${username}/coalitions`;
-  const { data } = await axios.get(coalitionUri, headers)
-    .catch(e => {
-      const error = new Error(e.message);
-      console.log(e.message);
-      error.status = e.response.status;
-      if (error.status === 401) {
-        res.redirect('/login/42');
-        return;
-      }
-      next(error);
-    })
-    .finally(() => { });
-  return (data[0]);
-};
-
 /* GET users listing. */
 router.get('/', ensureLoggedIn('/login/42'), async function (req, res, next) {
   const username = req.query.u;
@@ -38,28 +21,28 @@ router.get('/', ensureLoggedIn('/login/42'), async function (req, res, next) {
         { 'Authorization': 'Bearer ' + accessToken }
     }
     const uri = `${END_POINT_42_API}/v2/users/${username}`;
-    axios.get(uri, headers)
-      .then(async response => {
-        const one = {...response.data, 'coalition': await get42UserCoalition(username, headers)};
-        ObjectUtils.calcDiff(one.projects_users, 'marked_at');
-        await User.save(one);
-        res.render('user', { 
-          user: one, 
-          updatedAt: dayjs().format('YYYY/MM/DD HH:mm:ss'), 
-          dayjs 
-        });
-      })
-      .catch(e => {
-        const error = new Error(e.message);
-        console.log(e.message);
-        error.status = e.response.status;
-        if (error.status === 401) {
-          res.redirect('/login/42');
-          return;
-        }
-        next(error);
-      })
-      .finally(() => { });
+    const coalitionUri = `${END_POINT_42_API}/v2/users/${username}/coalitions`;
+
+    try {
+      const response = await axios.all([axios.get(uri, headers), axios.get(coalitionUri, headers)]);
+      const one = {...response[0].data, 'coalition': response[1].data[0]};
+      ObjectUtils.calcDiff(one.projects_users, 'marked_at');
+      await User.save(one);
+      res.render('user', { 
+        user: one, 
+        updatedAt: dayjs().format('YYYY/MM/DD HH:mm:ss'), 
+        dayjs 
+      });
+    } catch(e) {
+      const error = new Error(e.message);
+      console.log(e.message);
+      error.status = e.response.status;
+      if (error.status === 401) {
+        res.redirect('/login/42');
+        return;
+      }
+      next(error);
+    }
   } else {
     const one = (typeof user.data === 'string') ? JSON.parse(user.data) : user.data;
     ObjectUtils.calcDiff(one.projects_users, 'marked_at');
